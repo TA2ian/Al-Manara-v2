51b50dbc8f412886969ece8c28499ef1d8a49c78
TA2ian <65396854+TA2ian@users.noreply.github.com>
2026-09-04 09:03:20 +0400

    test(db): gate customer wallet persistence APIs

--- tests/database/003_contract_gate.sql
+++ tests/database/003_contract_gate.sql
@@ -1,6 +1,6 @@
 begin;
 
-select plan(2);
+select plan(4);
 
 select ok(
   (select count(*) from pg_proc where proname = 'reserve_receipt_submission') = 1,
@@ -10,6 +10,16 @@ select ok(
 select ok(
   (select count(*) from pg_proc where proname = 'finalize_receipt_submission') = 1,
   'canonical receipt finalization RPC exists'
+);
+
+select ok(
+  (select count(*) from pg_proc where proname = 'list_verified_wallets_for_telegram_user') = 1,
+  'verified wallet listing RPC exists'
+);
+
+select ok(
+  (select count(*) from pg_proc where proname = 'disable_wallet_for_telegram_user') = 1,
+  'wallet disable RPC exists'
 );
 
 select * from finish();
