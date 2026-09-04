3b5d653ab71c9a3e42a4e52cde0b840acde49005
TA2ian <65396854+TA2ian@users.noreply.github.com>
2026-09-05 00:13:27 +0400

    test(db): enforce pending payment creation state

---
+++ tests/database/005_order_creation_status.sql
@@ -0,0 +1,16 @@
+begin;
+
+select plan(1);
+
+select ok(
+    position("'PENDING_PAYMENT'" in pg_get_functiondef((
+        select p.oid
+        from pg_proc p
+        where p.proname = 'create_purchase_order_atomic'
+        limit 1
+    ))) > 0,
+    'atomic purchase-order creation enters the canonical PENDING_PAYMENT state'
+);
+
+select * from finish();
+rollback;
