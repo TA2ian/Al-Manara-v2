7399028bd269f59cc785b2a3bee1636b52bec831
TA2ian <65396854+TA2ian@users.noreply.github.com>
2026-09-06 12:57:26 +0400

    fix: correct fulfillment listing contract test

--- tests/database/006_admin_fulfillment_claim_owner.sql
+++ tests/database/006_admin_fulfillment_claim_owner.sql
@@ -23,7 +23,7 @@ select ok(
 );
 
 select ok(
-    position("'fulfillment'" in pg_get_functiondef((
+    position('fulfillment' in pg_get_functiondef((
         select p.oid
         from pg_proc p
         where p.proname = 'list_admin_orders'
